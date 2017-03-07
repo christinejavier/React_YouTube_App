d5ed1db35a36cbbfab9c664a43524ca572ace419
Christine Javier <christine@still-water.com>
2017-03-06 18:00:05 -0800

    adds event handler on input element to console.log the input values

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,1 +1,13 @@
-//YOUR GOAL IS TO CREATE AN INPUT BOX THAT IS CAPABLE OF KEEPING TRACK OF STATE. ALSO, IT NEEDS AN EVENT HANDLER
+import React, {Component} from 'react';
+
+class SearchBar extends Component {
+  constructor(props){
+    super(props);
+    this.state = {term: ''};
+  }
+  render(){
+    return <input onChange={(event) => console.log(event.target.value)}/>
+  }
+}
+
+export default SearchBar;
