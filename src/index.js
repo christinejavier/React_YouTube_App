05591d5cd8c2760659bf900abfa2af23ccdbda49
Christine Javier <christine@still-water.com>
2017-03-15 20:28:08 -0700

    integrates youtube API, and renders a response of the thumbnail of the video as well as its description

--- src/index.js
+++ src/index.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+const API_KEY = "AIzaSyC9t-SsA3Gw1gNJ2oJ1oLbdAMY86sBEXnE";
+
 
 // create a new component that produces html
-const App = function(){
-  return <div>Hi</div>;
+class App extends Component{
+  constructor(props){
+    super(props);
+    this.state = {videos:[]};
+    YTSearch({key: API_KEY, term:"surfboards"}, (videos) => {
+      // this.setState({videos:videos});
+      this.setState({videos});
+    });
+  }
+  render(){
+    return (
+      <div>
+        <SearchBar/>
+        <VideoList videos={this.state.videos}/>
+      </div>
+    );
+  }
 }
 
 
 // get generated html from component, and inject it into DOM
-ReactDOM.render(App);
+ReactDOM.render(<App/>, document.querySelector(".container"));
