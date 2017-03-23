190022f5f1bcbed4d816f351e27b3ae19efd050d
Christine Javier <christine@still-water.com>
2017-03-22 20:38:05 -0700

    adds click event so that users can choose video to watch

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 const API_KEY = "AIzaSyC9t-SsA3Gw1gNJ2oJ1oLbdAMY86sBEXnE";
 
 
@@ -10,17 +11,26 @@ const API_KEY = "AIzaSyC9t-SsA3Gw1gNJ2oJ1oLbdAMY86sBEXnE";
 class App extends Component{
   constructor(props){
     super(props);
-    this.state = {videos:[]};
+    this.state = {
+      videos:[],
+      selectedVideo: null
+    };
     YTSearch({key: API_KEY, term:"surfboards"}, (videos) => {
       // this.setState({videos:videos});
-      this.setState({videos});
+      this.setState({
+        videos:videos,
+        selectedVideo:videos[0]
+      });
     });
   }
   render(){
     return (
       <div>
         <SearchBar/>
-        <VideoList videos={this.state.videos}/>
+        <VideoDetail video={this.state.selectedVideo}/>
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+          videos={this.state.videos}/>
       </div>
     );
   }
